183768cf524c663a6f05c85508739ab9d63d6de5
Diego G. Dreossi <dididreo@hotmail.com>
2022-03-19 20:41:25 -0300

    formularios de livros sem salvar o cadastro

--- livro/forms.py
+++ livro/forms.py
@@ -7,3 +7,10 @@ class CadastroLivro(forms.ModelForm):
         model = Livro
         fields = "__all__"
         #fields = ("nome","autor","co_autor","data_cadastro","emprestado","categoria","sinopse",)
+
+    # Construtor
+    def __init__(self,*args, **kwargs):
+        # Permite que os métodos que precisamos de formulário ainda funcionem nessa sobrescrição (Polimorfismo)
+        super().__init__()
+        # Define o componente do campo do formulário como um elemento escondido
+        self.fields['usuario'].widget = forms.HiddenInput()
