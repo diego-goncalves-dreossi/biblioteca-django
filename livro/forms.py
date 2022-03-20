6dd2f689f8f5ae223204a8993d20af5c11f35f6c
diego-goncalves-dreossi <dididreo@hotmail.com>
2022-03-20 16:24:55 -0300

    campo usuario hidden form cadastrarlivro

--- livro/forms.py
+++ livro/forms.py
@@ -7,3 +7,12 @@ class CadastroLivro(forms.ModelForm):
         model = Livro
         fields = "__all__"
         #fields = ("nome","autor","co_autor","data_cadastro","emprestado","categoria","sinopse",)
+
+
+    # Construtor
+    def __init__(self,*args,**kwargs):
+        # Permite rodas as funcionalidades basicas de formulário que precisamos
+        super().__init__(*args,**kwargs)
+
+        # Define o componente do campo do formulário como um elemento escondido
+        self.fields['usuario'].widget = forms.HiddenInput()
